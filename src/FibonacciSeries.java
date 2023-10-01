d7ef9c26588874c7734cd25f5ad77d2c48b07314
Sooryajagadeesan <sooryajagadeesan@gmail.com>
2023-10-01 12:47:51 +0530

    Worked on the basic version of generating fibonacci series till N

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the size of the fibonacci series: ");
+        int limit = in.nextInt();
+
+//        When the user enters a negative number for limit, Prompt the user of a valid non-negative number for limit
+        while(limit < 0) {
+            System.out.println("Oops! Invalid limit value, Please enter a non-negative limit value");
+            limit = in.nextInt();
+        }
+
+        int[] fibonacciSeries = generateFibonacciSeries(limit);
+        System.out.println("Fibonacci series of size " + limit + ": " + Arrays.toString(fibonacciSeries));
+    }
+
+    static int[] generateFibonacciSeries(int limit) {
+        if(limit <= 0) {
+//            Return an empty array when the limit value is less than or equal to 0
+            return new int[]{};
+        }
+
+        int[] fibonacciSeries = new int[limit];
+
+        int first = 0;
+        int second = 1;
+
+        fibonacciSeries[0] = first;
+        if (limit == 1) {
+//            Return the array with 1 value when the limit is 1
+            return fibonacciSeries;
+        }
+
+        fibonacciSeries[1] = second;
+        int startIndex = 2;
+
+//        The below loop will execute when the limit value is greater than 2.
+        while (startIndex < limit) {
+            int next = first + second;
+            first = second;
+            second = next;
+
+            fibonacciSeries[startIndex] = next;
+            startIndex++;
+        }
+
+        return fibonacciSeries;
+    }
+}
